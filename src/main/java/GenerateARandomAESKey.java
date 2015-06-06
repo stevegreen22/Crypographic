6a36d12f3e2460e0a35710c36d9499920bd7d89d
Steve <Stevegreen22@me.com>
2015-06-06 10:09:28 +0100

    Initial Commit - Functional AES Encryption Example

---
+++ src/main/java/GenerateARandomAESKey.java
@@ -0,0 +1,48 @@
+/**
+ * Created by SteveGreen on 06/06/15.
+ */
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import java.io.*;
+
+
+public class GenerateARandomAESKey {
+
+    public static byte[] encryptAMessageWithAes(String message, SecretKey key, IvParameterSpec iv)
+            throws Exception{
+        ByteArrayOutputStream out = new  ByteArrayOutputStream();
+        Cipher aes = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        aes.init(Cipher.ENCRYPT_MODE, key, iv);
+        CipherOutputStream cipherOut = new CipherOutputStream(out, aes);
+        OutputStreamWriter writer = new OutputStreamWriter(cipherOut);
+
+        try{
+            writer.write(message);
+        }finally{
+            writer.close();
+        }
+
+        return out.toByteArray();
+    }
+
+    public static String decryptAMessageWithAes(byte[] cipherText, SecretKey jey, IvParameterSpec iv)
+        throws Exception{
+
+        ByteArrayInputStream in = new ByteArrayInputStream(cipherText);
+        Cipher aes = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        aes.init(Cipher.DECRYPT_MODE, jey, iv);
+        CipherInputStream cipherInputStream = new CipherInputStream(in, aes);
+        InputStreamReader reader = new InputStreamReader(cipherInputStream);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+
+        try{
+            return bufferedReader.readLine();
+        }finally{
+            bufferedReader.close();
+        }
+    }
+
+}
---
+++ src/test/java/GenerateARandomAesKeyTest.java
@@ -0,0 +1,47 @@
+import org.junit.Test;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+
+import java.security.SecureRandom;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by SteveGreen on 06/06/15.
+ */
+public class GenerateARandomAesKeyTest {
+
+
+    @Test
+    public void generateARandomKey() throws Exception{
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(256);
+        SecretKey key = keyGenerator.generateKey();
+
+        assertEquals("AES", key.getAlgorithm());
+        assertEquals(32, key.getEncoded().length);
+    }
+
+    @Test
+    public void encryptAMessageWithAes() throws Exception{
+        String message = "I know the secret!";
+
+        //instance
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128);
+        SecretKey key = keyGenerator.generateKey();
+
+        //IV
+        SecureRandom random = new SecureRandom();
+        byte[] buffer = new byte[16];
+        random.nextBytes(buffer);
+        IvParameterSpec iv = new IvParameterSpec(buffer);
+
+        byte[] cipherText = GenerateARandomAESKey.encryptAMessageWithAes(message, key, iv);
+        String actualMessage = GenerateARandomAESKey.decryptAMessageWithAes(cipherText, key, iv);
+
+        assertEquals(message, actualMessage);
+    }
+}
